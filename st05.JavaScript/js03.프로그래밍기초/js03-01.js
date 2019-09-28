6c4c809eda6d87bdffc0f040fb47f8b6a3a3f963
dfsafasdf <fdsafadwfa@FAFAFf.alclfa>
2019-09-28 17:53:36 +0900

    class

--- st05.JavaScript/js03.프로그래밍기초/js03-01.js
+++ st05.JavaScript/js03.프로그래밍기초/js03-01.js
@@ -1,6 +1,10 @@
 "use strict";
 
+/* 대소문자 구분 */
 var i = 10;
-var I = 10;
+var I = 10; // 숫자, Number
+/* 큰 따음표가 있는 경우는 문자.  */
+var I = "10"; // 문자열, string
+var I = true; // 불린 (있다, 없다) Boolean
 
 
